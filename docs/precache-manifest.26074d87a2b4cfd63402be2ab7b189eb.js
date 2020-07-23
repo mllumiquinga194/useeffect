014d2733d7680be2f6798604f6d998f6fb04d55e
Marcos Llumiquinga <mallth194@gmail.com>
2020-07-23 19:43:46 -0400

    docs agregado

---
+++ docs/precache-manifest.26074d87a2b4cfd63402be2ab7b189eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd03320c6abd9cb517b0e55eaaed4925",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f352c776eade51eb3c6",
+    "url": "/static/css/main.6e43afe3.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f352c776eade51eb3c6",
+    "url": "/static/js/main.66188530.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
